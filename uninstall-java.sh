102247eac3902708a4305590b3b1528caef3e639
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2017-05-02 15:37:11 +0530

    Use spaces instead of tabs. Check plugins directory for Mozilla

--- uninstall-java.sh
+++ uninstall-java.sh
@@ -45,8 +45,8 @@ confirm () {
 
 # Make sure the script is running as root.
 if [ "$UID" -ne "0" ]; then
-	echo "You must be root to run $0. Try following"; echo "sudo $0"; 
-	exit 9
+    echo "You must be root to run $0. Try following"; echo "sudo $0";
+    exit 9
 fi
 
 
@@ -74,14 +74,16 @@ fi
 commands=( "jar" "java" "javac" "javadoc" "javah" "javap" "javaws" "jcmd" "jconsole" "jarsigner" "jhat" "jinfo" "jmap" "jmc" "jps" "jstack" "jstat" "jstatd" "jvisualvm" "keytool" "policytool" "wsgen" "wsimport" )
 
 if (confirm "Run update-alternatives commands?"); then
-	echo "Running update-alternatives --remove for ${commands[@]} mozilla-javaplugin.so"
+    echo "Running update-alternatives --remove for ${commands[@]} mozilla-javaplugin.so"
 
-	for i in "${commands[@]}"
-	do
-		sudo update-alternatives --remove "$i" "$java_dist_dir/bin/$i"
-	done
+    for i in "${commands[@]}"
+    do
+        sudo update-alternatives --remove "$i" "$java_dist_dir/bin/$i"
+    done
 
-	sudo update-alternatives --remove "mozilla-javaplugin.so" "$java_dist_dir/jre/lib/amd64/libnpjp2.so"
+    if [[ -d "/usr/lib/mozilla/plugins/" ]]; then
+        sudo update-alternatives --remove "mozilla-javaplugin.so" "$java_dist_dir/jre/lib/amd64/libnpjp2.so"
+    fi
 fi
 
 if (confirm "Remove directory '$java_dist_dir'?"); then
